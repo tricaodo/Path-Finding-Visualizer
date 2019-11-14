59c6d343a4137b3fec0eb3a1ddb3b71e7c6c117e
Tri Do <trido1903@gmail.com>
2019-11-14 10:29:51 -0800

    refactor the update function

--- src/Grid.java
+++ src/Grid.java
@@ -82,20 +82,9 @@ public class Grid extends JPanel implements MouseListener, ActionListener, Mouse
                     int cost = (int) Math.floor(Math.random() * 15);
                     grids[col][row].getEdges().add(new Edge(cost, grids[col][row + 1]));
                 }
-
-
-
-
-
-
-
             }
         }
         System.out.println("Width: " + grids.length + ", Height" + grids[0].length);
-//        startVertex = grids[10][10];
-//        startVertex.setStyle(4);
-//        endVertex = grids[22][20];
-//        endVertex.setStyle(5);
     }
 
     public void start(String str) {
@@ -140,15 +129,15 @@ public class Grid extends JPanel implements MouseListener, ActionListener, Mouse
         int x = e.getX() / DIMENSION; // get the x position of the grid being dragged.
         int y = e.getY() / DIMENSION; // get the y position of the grid being dragged.
 
-        if(keyFlag == 0){
+        if (keyFlag == 0) {
             startVertex = grids[x][y];
             startVertex.setStyle(4); // set the grid is the start.
             keyFlag++;
-        }else if(keyFlag == 1){
+        } else if (keyFlag == 1) {
             endVertex = grids[x][y];
             endVertex.setStyle(5); // set the grid is the end.
             keyFlag++;
-        }else{
+        } else {
             grids[x][y].setStyle(3); // set the grid is the wall.
         }
         repaint();
@@ -193,30 +182,11 @@ public class Grid extends JPanel implements MouseListener, ActionListener, Mouse
 
     class PathFinding extends SwingWorker<Void, Void> {
 
-        private static final long DELAY = 5;
-
-        public void delay() {
-            try {
-                Thread.sleep(DELAY); //simulate long process
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
-        }
-
-        private void delay(int delay) {
-            try {
-                Thread.sleep(delay); //simulate long process
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
-        }
-
-
         @Override
         public Void doInBackground() {
-            if(algorithmStr.equals("Breadth First Search")){
+            if (algorithmStr.equals("Breadth First Search")) {
                 BFS();
-            }else{
+            } else {
                 DFS();
             }
             return null;
@@ -234,52 +204,45 @@ public class Grid extends JPanel implements MouseListener, ActionListener, Mouse
             startVertex.setVisited(true);
             Vertex targetVertex = null;
 
-            while(!stack.isEmpty() && !isFinished){
+            while (!stack.isEmpty() && !isFinished) {
                 Vertex current = stack.pop();
-                if(current != startVertex){
+                if (current != startVertex) {
                     current.setStyle(0);
                 }
-                repaint();
-                delay();
-                for(Edge edge: current.getEdges()){
-                    if(!visited.contains(edge.getDestination()) && edge.getDestination().getStyle() != 3){
+                update(5);
+                for (Edge edge : current.getEdges()) {
+                    if (!visited.contains(edge.getDestination()) && edge.getDestination().getStyle() != 3) {
                         edge.getDestination().setPrevious(current); // point the pointer to the previous node.
-                        if(edge.getDestination() == endVertex){
+                        if (edge.getDestination() == endVertex) {
                             targetVertex = endVertex;
-                            targetVertex.setVisited(true);
                             isFinished = true;
                             break;
                         }
                         edge.getDestination().setStyle(1);
                         stack.push(edge.getDestination()); // push to the stack.
                         visited.add(edge.getDestination()); // add to visited.
-                        repaint();
-                        delay();
+                        update(5);
                     }
                 }
             }
-            if(targetVertex != null){
-                while(targetVertex != null){
-                    if(targetVertex == startVertex){
+            while (!stack.isEmpty()) {
+                Vertex current = stack.pop();
+                current.setStyle(0);
+                update(5);
+            }
+            if (targetVertex != null) {
+                while (targetVertex != null) {
+                    if (targetVertex == startVertex) {
                         break;
                     }
-                    if(targetVertex != endVertex){
+                    if (targetVertex != endVertex) {
                         targetVertex.setStyle(2);
                         targetVertex.setVisited(true);
                     }
                     targetVertex = targetVertex.getPrevious();
-                    repaint();
-                    delay(10);
+                    update(10);
                 }
-                for(int i = 0; i < grids.length; i++){
-                    for(int j = 0; j < grids[i].length; j++){
-                        if(!grids[i][j].isVisited() && grids[i][j].getStyle() != 3){
-                            grids[i][j].setStyle(0);
-                            repaint();
-                        }
-                    }
-                }
-            }else{
+            } else {
                 System.out.println("No Path!!!");
             }
         }
@@ -296,8 +259,7 @@ public class Grid extends JPanel implements MouseListener, ActionListener, Mouse
                 if (current != startVertex) {
                     current.setStyle(0);
                 }
-                repaint();
-                delay();
+                update(5);
                 for (Edge edge : current.getEdges()) {
                     // check whether the neighbors are visited and those vertices are not the wall.
                     if (!visited.contains(edge.getDestination()) && edge.getDestination().getStyle() != 3) {
@@ -310,8 +272,7 @@ public class Grid extends JPanel implements MouseListener, ActionListener, Mouse
                         edge.getDestination().setStyle(1);
                         queue.offer(edge.getDestination());
                         visited.add(edge.getDestination());
-                        repaint();
-                        delay();
+                        update(5);
                     }
                 }
             }
@@ -327,12 +288,20 @@ public class Grid extends JPanel implements MouseListener, ActionListener, Mouse
                         targetNode.setStyle(2);  // change color of the vertices except the target vertex.
                     }
                     targetNode = targetNode.getPrevious();
-                    repaint();
-                    delay(10);
+                    update(10);
                 }
             } else {
                 System.out.println("No Path!!!");
             }
+        }
+    }
+
+    private void update(int delay) {
+        repaint();
+        try {
+            Thread.sleep(delay); //simulate long process
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
         }
     }
 }
