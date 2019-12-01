535dfd615dc4accefe3337f3a61024967ab569ae
Tri Do <trido1903@gmail.com>
2019-11-30 23:18:14 -0800

    shorten the code

--- src/Vertex.java
+++ src/Vertex.java
@@ -35,16 +35,8 @@ public class Vertex {
         return x;
     }
 
-    public void setX(int x) {
-        this.x = x;
-    }
-
     public int getY() {
         return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
     }
 
     public int getStyle() {
